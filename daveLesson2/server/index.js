62556ae1061489e6de328558f9299bfe15b64bfc
Surya Prakash Singh <suryaprakashsingh110@gmail.com>
2024-01-16 22:38:15 +0530

    lesson 2 dave gray added

---
+++ daveLesson2/server/index.js
@@ -0,0 +1,26 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*", // means no one is blocked
+    // origin:
+    // process.env.NODE_ENV === "production" ? false : ["http://localhost:5500","http://127.0.0.1:5500"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id} connected`);
+
+  socket.on("message", (data) => {
+    // console.log(data);
+    io.emit("message", `${socket.id}: ${data}`);
+  });
+  
+});
+
+httpServer.listen(3500, () => {
+  console.log("Listening on port 3500");
+});
