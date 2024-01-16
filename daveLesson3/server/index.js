6161a8a3e62f4d431ab1cd4971f0d6fbdd5e06c5
Surya Prakash Singh <suryaprakashsingh110@gmail.com>
2024-01-16 22:56:58 +0530

    lecture 3 added

--- daveLesson3/server/index.js
+++ daveLesson3/server/index.js
@@ -1,13 +1,24 @@
-const { createServer } = require("http");
+const express = require("express");
 const { Server } = require("socket.io");
+const path = require("path");
+const { fileURLToPath } = require("url");
 
-const httpServer = createServer();
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const io = new Server(httpServer, {
+const PORT = process.env.PORT || 3500;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const expressServer = app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
+});
+
+const io = new Server(expressServer, {
   cors: {
     origin: "*", // means no one is blocked
-    // origin:
-    // process.env.NODE_ENV === "production" ? false : ["http://localhost:5500","http://127.0.0.1:5500"],
   },
 });
 
@@ -18,9 +29,4 @@ io.on("connection", (socket) => {
     // console.log(data);
     io.emit("message", `${socket.id}: ${data}`);
   });
-  
-});
-
-httpServer.listen(3500, () => {
-  console.log("Listening on port 3500");
 });
