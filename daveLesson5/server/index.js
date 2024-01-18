f120662cf172f6438400832aeb59e35fd7c5a5cf
surya.singh <surya.singh@diatoz.com>
2024-01-18 19:33:08 +0530

    lecture 5 in progress

--- daveLesson5/server/index.js
+++ daveLesson5/server/index.js
@@ -8,6 +8,8 @@ const { fileURLToPath } = require("url");
 
 const PORT = process.env.PORT || 3500;
 
+const ADMIN = "Admin";
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -15,6 +17,7 @@ app.use(express.static(path.join(__dirname, "public")));
 const expressServer = app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
 });
+
 
 const io = new Server(expressServer, {
   cors: {
@@ -26,7 +29,7 @@ io.on("connection", (socket) => {
   console.log(`User ${socket.id} connected`);
 
   // upon connection inform only to user
-  socket.emit("message", "Welcome to Chat App");
+  socket.emit("message",buildMsg(ADMIN,"Welcome to Chat App"));
 
   // Upon Connection inform to all others except that particular user
   socket.broadcast.emit("message", `User ${socket.id} connected`);
@@ -50,3 +53,52 @@ io.on("connection", (socket) => {
   });
 
 });
+
+// State for users
+const UsersState = {
+  users:[],
+  setUsers: function(newUsersArray){
+    this.users= newUsersArray
+  }
+}
+
+
+function buildMsg(name,text){
+  return {
+    name,
+    text,
+    time: new Intl.DateTimeFormat('default',{
+      hour:"numeric",
+      minute:"numeric",
+      second:"numeric"
+    }).format(new Date())
+  }
+}
+
+//  User Functions
+function activateUser(id,name,room){
+ const user = {id,name,room}
+ UsersState.setUsers([
+    ...UsersState.users.filter(user => user.id !== id),
+    user
+ ])
+ return user
+}
+
+function userLeavesApp(id){
+  UsersState.setUsers([
+   UsersState.users.filter(user => user.id !== id)
+ ])
+}
+
+function getUser(id){
+  return UsersState.users.find(user => user.id === id)
+}
+
+function getUsersInRoom(room){
+  return UsersState.users.find(user => user.room === room)
+}
+
+function getAllActiveRooms(){
+  return Array.from(new Set(UsersState.users.map(user => user.room)))
+}
